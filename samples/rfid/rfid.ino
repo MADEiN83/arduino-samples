2e1d3191133ece13a39f005145c0effceede32bb
Anthony DI STEFANO <anthony.distefano@hotmail.fr>
2019-05-19 19:29:46 +0200

    Upgrading RFID script

--- samples/rfid/rfid.ino
+++ samples/rfid/rfid.ino
@@ -1,9 +1,11 @@
 #include <SPI.h>
 #include <MFRC522.h>
+
 // Affectation des broches
 #define RST_PIN 9
 #define SS_PIN 10
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup()
 {
@@ -11,33 +13,34 @@ void setup()
   Serial.begin(9600);
   while (!Serial)
     ;
+
   SPI.begin();
-  mfrc522.PCD_Init();
-  mfrc522.PCD_DumpVersionToSerial(); // Affichage des données de la bibliothèque
+  rfid.PCD_Init();
+  rfid.PCD_DumpVersionToSerial(); // Affichage des données de la bibliothèque
   Serial.println(F("Scan PICC to see UID, type, and data blocks..."));
 }
 
 void loop()
 {
   // Attente d'une carte RFID
-  if (!mfrc522.PICC_IsNewCardPresent())
+  if (!rfid.PICC_IsNewCardPresent())
   {
     return;
   }
 
   // Récupération des informations de la carte RFID
-  if (!mfrc522.PICC_ReadCardSerial())
+  if (!rfid.PICC_ReadCardSerial())
   {
     return;
   }
 
   Serial.print(F("The NUID tag is: "));
-  String nuid = getCardUNID(mfrc522.uid.uidByte, mfrc522.uid.size);
+  String nuid = getCardUNID(rfid.uid.uidByte, rfid.uid.size);
   Serial.println(nuid);
   Serial.println();
 
   // Affichage des informations de la carte RFID
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  rfid.PICC_DumpToSerial(&(rfid.uid));
 }
 
 /**
