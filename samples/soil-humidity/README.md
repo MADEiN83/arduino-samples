b78f5e096dcbf0ede407999ecf0a84d05363d6ac
Anthony DI STEFANO <anthony.distefano@hotmail.fr>
2019-05-19 18:17:39 +0200

    Soil humidity

---
+++ samples/soil-humidity/README.md
@@ -0,0 +1,14 @@
+# soil humidity
+
+## map
+
+https://www.arduino.cc/reference/en/language/functions/math/map/
+
+## Pin-out
+
+The FC-28 soil moisture sensor has four pins:
+
+- VCC: Power
+- A0: Analog Output
+- D0: Digital Output
+- GND: Ground
---
+++ samples/soil-humidity/soil-humidity.ino
@@ -0,0 +1,26 @@
+int pin = A0;
+int limit = 300; 
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    int value = analogRead(pin);
+    value = map(value, 1023, 0, 0, 100);
+
+    Serial.print("Mositure : ");
+    Serial.print(value);
+    Serial.println("%");
+
+    if(value > 50) {
+      digitalWrite(13, HIGH);
+    } else {
+      digitalWrite(13, LOW);
+    }
+    
+    delay(200);
+}
